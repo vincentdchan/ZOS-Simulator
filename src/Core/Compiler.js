386b6e24aeb7ff0f67bfa20fc935fc9f6585cd87
ChannelOne <cdzos97@gmail.com>
2017-08-14 16:46:14 +0800

    Regex compilor

--- src/Core/Compiler.js
+++ src/Core/Compiler.js
@@ -4,21 +4,33 @@
  ************************/ 
 import {Program} from "./Program"
 
-let regex_line = /([a-zA-Z]+)\s+([RC]?\d+)\s*,\s*([RC]?\d+)\s*,\s*([RC]?\d+)\s*(;.*)?/;
+let regex_line = /([a-zA-Z]+)\s+([RC]?\d+)\s*,\s*([RC]?\d+|\s+)\s*,\s*([RC]?\d+|\s+)\s*(;.*)?/;
+let regex_define = /(define|DEFINE)\s+(\w+)\s+"((\\"|\w)+)"/
 let regex_comment = /\s*(;.*)?/;
 
 export function Compile(source_code) {
     let lines = source_code.split("\n");
+    let program = new Program();
+
+    let source_map;
 
     for (let i = 0; i < lines.length; i++) {
         let line = lines[i];
         let result;
         if ((result = regex_line.exec(line)) !== null) {
-
+            let inst_id = program.AddInstruction(result[1], result[2], result[3], result[4]);
+            source_map[inst_id] = i;
+        } else if ((result = regex_define.exec(line)) != null) {
+            let define_name = result[2],
+                define_value = result[3];
         } else if ((result = regex_comment.exec(line)) !== null) {
-
+            // comment line, ignore it
         } else {
             throw new Error("Compile error: " + line);
         }
     }
+
+    program.source_map = source_map;
+
+    return program;
 }
--- src/Core/Program.js
+++ src/Core/Program.js
@@ -1,18 +1,27 @@
 
+let program_id = 0;
+
+function newInst(op_code, a1, a2, a3) {
+    return [op_code, a1, a2, a3];
+}
+
 export class Program {
 
     constructor() {
+        this._name = "program#" + program_id++;
         this._constants = [];
         this._instructions = [];
+        this._source_map = null;
     }
 
     AddInstruction(op_code, a1, a2, a3) {
         this._instructions.push(newInst(op_code, a1, a2, a3));
+        return this._instructions.length - 1;
     }
 
     AddConstant(constant) {
         this._constants.push(constant);
-        return this._constants.length;
+        return this._constants.length - 1;
     }
 
     get constants() {
@@ -21,6 +30,22 @@ export class Program {
 
     get instructions() {
         return this._instructions;
+    }
+
+    get source_map() {
+        return this._source_map;
+    }
+
+    set source_map(value) {
+        this._source_map = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
     }
 
 }
